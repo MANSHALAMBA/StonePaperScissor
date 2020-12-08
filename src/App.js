43e0dcf45a63d0bd11e6d15748103c3743132f51
Lambamansha <lambamadhu555@gmail.com>
2020-12-08 09:03:19 +0530

    version 1.0

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import Controller from "./containers/controller/controller";
+import { Typography } from "antd";
+
+import "antd/dist/antd.css";
+
+import "./App.css";
+const { Title } = Typography;
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Title level={3} style={{ textAlign: "center" }}>
+          Stone Paper Scissor
+        </Title>
+        <Controller />
+      </div>
+    );
+  }
+}
+
+export default App;
